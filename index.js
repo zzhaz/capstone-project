c8fd5e642ed58be8a2c9c8bfacb0441571625779
zzhaz <128551624+zzhaz@users.noreply.github.com>
2023-03-25 19:44:12 +0000

    save carousel

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Hello world!");
+
+// Carousel function
+function initializeCarousel() {
+    const carousel = document.getElementById('carousel');
+    const images = [...carousel.getElementsByTagName('img')];
+    const prevButton = document.getElementById('carousel-control-prev');
+    const nextButton = document.getElementById('carousel-control-next');
+
+    let index = 0;
+
+    function updateCarousel() {
+        for (let image of images) {
+            image.classList.add('hidden');
+        }
+        images[index].classList.remove('hidden');
+    }
+
+    function nextImage() {
+        index = (index + 1) % images.length;
+        updateCarousel();
+    }
+
+    function prevImage() {
+        index = (index - 1 + images.length) % images.length;
+        updateCarousel();
+    }
+
+    prevButton.addEventListener('click', prevImage);
+
+    nextButton.addEventListener('click', nextImage);
+
+    updateCarousel();
+}
+
+// On page load
+document.addEventListener("DOMContentLoaded", () => {
+    initializeCarousel();
+});
