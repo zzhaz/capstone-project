4db0cf4c48d987a0ba738fe6fd52b8df8a18c26d
zzhaz <128551624+zzhaz@users.noreply.github.com>
2023-03-29 00:05:51 +0000

    message

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 // Carousel function
 function initializeCarousel() {
     const carousel = document.getElementById('carousel');
+    if (!carousel) {
+        return;
+    }
+
     const images = [...carousel.getElementsByTagName('img')];
     const prevButton = document.getElementById('carousel-control-prev');
     const nextButton = document.getElementById('carousel-control-next');
@@ -32,7 +36,42 @@ function initializeCarousel() {
     updateCarousel();
 }
 
+function initializePopup(){
+    const popup = document.getElementById('popup');
+    const popupBg = document.getElementById('popup-bg');
+    const good = document.getElementById('good');
+    const bad = document.getElementById('bad');
+
+    if (!popup){
+        return;
+    }
+    popup.classList.remove('hidden');
+    
+    function closePopup(){
+        popup.classList.add('hidden');
+    }
+    
+    popupBg.addEventListener('click', closePopup);
+
+    function onGoodClick(){
+        good.classList.remove("fa-thumbs-up");
+        good.classList.add("fa-face-smile");
+  
+    }
+    good.addEventListener('click', onGoodClick);
+
+    function onBadClick(){
+        bad.classList.remove("fa-thumbs-down");
+        bad.classList.add("fa-face-sad-tear");
+  
+    }
+
+    bad.addEventListener('click', onBadClick);
+}
+
+
 // On page load
 document.addEventListener("DOMContentLoaded", () => {
     initializeCarousel();
+    setInterval(initializePopup, 10000);
 });
